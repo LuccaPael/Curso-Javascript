3073cf88dd19d6afa09e420c18066eb61f510708
LuccaPael <76496173+LuccaPael@users.noreply.github.com>
2023-03-06 14:07:15 -0300

    2 - Condicional - Propriedades I
    Diferença entre == e ===

--- Condicional.js
+++ Condicional.js
@@ -2,7 +2,6 @@ let nomeCarro = "Fiat";
 
 if (nomeCarro == "Ferrari"){
 	console.log("O seu carro é ferrari.");
-	
 } else{
 	console.log("O seu carro não é fiat");
 }
@@ -10,7 +9,6 @@ if (nomeCarro == "Ferrari"){
 
 
 let idade = 30;
-
 if (idade < 27){
 	console.log("Idade é menor que 27.");
 	
@@ -19,12 +17,22 @@ if (idade < 27){
 }
 
 
-
 let ano = 2025;
-
 if (ano <= 2027){
 	console.log("O ano é menor ou igual a 2027.");
 	
 } else{
 	console.log("O ano é maior que 2027.");
+}
+
+
+
+
+// Diferença entre == e === (verificação estrita)
+let idade = "20";
+
+if(idade === 20) {
+	console.log("O dado informado foi do tipo correto")	
+} else{
+	console.log("O dado informado não foi do tipo correto")
 }
