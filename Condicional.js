ad584889d7b3e655afed0f17e20542e404cd2169
LuccaPael <76496173+LuccaPael@users.noreply.github.com>
2023-03-06 13:58:31 -0300

    1 -  Inserindo Arquivos
    Inseri arquivos html e javascript (aula de condicionais - b7web)

---
+++ Condicional.js
@@ -0,0 +1,30 @@
+let nomeCarro = "Fiat";
+
+if (nomeCarro == "Ferrari"){
+	console.log("O seu carro é ferrari.");
+	
+} else{
+	console.log("O seu carro não é fiat");
+}
+
+
+
+let idade = 30;
+
+if (idade < 27){
+	console.log("Idade é menor que 27.");
+	
+} else{
+	console.log("Idade maior que 27.");
+}
+
+
+
+let ano = 2025;
+
+if (ano <= 2027){
+	console.log("O ano é menor ou igual a 2027.");
+	
+} else{
+	console.log("O ano é maior que 2027.");
+}
