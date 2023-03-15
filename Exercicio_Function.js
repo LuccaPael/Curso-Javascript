2f67637998f19cbc77bbc1bfd02c32f196986eba
LuccaPael <luccapael@hotmail.com>
2023-03-14 22:17:24 -0400

    Informações sobre array

--- Exercicio_Function.js
+++ Exercicio_Function.js
@@ -2,8 +2,21 @@
 
 //Dica: para calcular a média, some todos os valores do array e divida pelo tamanho do array.
 
-let numeros = [10, 20, 30, 40, 50];
+
+
+let numeros = [10, 20, 30, 40];
+
+// Adicionando elemento no array:  numeros.push('elemento')
+numeros.push(50);
 console.log(numeros);
+
+
+// Remover o último elemento do array:  numeros.pop('elemento')
+// Remover o primeiro elemento do array:  numeros.shift('elemento')
+
+
+tamanhoArray = numeros.length;
+console.log(tamanhoArray);
 
 function calculaMedia(numeros) {
     tamanhoArray = numeros.length;
@@ -17,3 +30,5 @@ function calculaMedia(numeros) {
 
 // let media = calculaMedia(numeros)
 // console.log(media)
+
+
