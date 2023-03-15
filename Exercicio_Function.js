4535488367f16a02823f973206fed694f4a1e79f
LuccaPael <luccapael@hotmail.com>
2023-03-14 22:08:12 -0400

    Aprendendo sobre array - pt.1

---
+++ Exercicio_Function.js
@@ -0,0 +1,19 @@
+//Crie uma função chamada calculaMedia que recebe um array de números como parâmetro e retorna a média dos valores.
+
+//Dica: para calcular a média, some todos os valores do array e divida pelo tamanho do array.
+
+let numeros = [10, 20, 30, 40, 50];
+console.log(numeros);
+
+function calculaMedia(numeros) {
+    tamanhoArray = numeros.length;
+
+
+
+
+
+
+}
+
+// let media = calculaMedia(numeros)
+// console.log(media)
