d9c7049e86fb60cbc132c599b909b68e131d5919
Lucca <76496173+LuccaPael@users.noreply.github.com>
2023-03-22 19:14:23 -0300

    Criei Arrow Function

---
+++ Arrow_Function.js
@@ -0,0 +1,31 @@
+// Function
+function somar(x,y) {
+  return x + y; 
+}
+
+console.log(somar(10, 5));
+
+
+// Arrow Function
+const subtrair = (x, y) => {
+    return x - y;
+}
+console.log (subtrair(10, 5));
+
+
+
+// Arrow Function na mesma linha (sem return)
+const dividir = (x, y) => x/y;
+console.log (dividir(10, 5));
+
+
+// Arrow Function c/ um parâmetro apenas
+const sobrenome = (name) => 'Lucca ' + sob;
+console.log(sobrenome('Pael'));
+
+
+
+
+
+
+
