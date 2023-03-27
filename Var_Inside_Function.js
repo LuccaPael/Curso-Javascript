55cdae51f93cdd5aba12fb543a8f903d6ccb6693
LuccaPael <luccapael@hotmail.com>
2023-03-26 21:38:04 -0400

    Update Var_Inside_Function.js

--- Var_Inside_Function.js
+++ Var_Inside_Function.js
@@ -1,9 +1,8 @@
 // Criando uma variável de escopo global
-
 let count = 0;
 
 function add() {
-    count++      
+    count++;      
 }
 
 add();
@@ -13,3 +12,18 @@ console.log(count);
 
 
 
+// Criando uma variável de escopo local (dentro da function)
+let contador = 0;
+
+function add() {
+    let contador = 0;
+    contador++
+}
+
+add()
+add()
+
+console.log(contador); 
+
+
+// A variável de escopo global tem preferência sobre uma função de escopo global de mesmo nome
