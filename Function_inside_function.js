d14ef4ee82847685f191eef3b062514ed7b8ec90
LuccaPael <luccapael@hotmail.com>
2023-03-29 23:52:30 -0400

    Create Function_inside_function.js

---
+++ Function_inside_function.js
@@ -0,0 +1,12 @@
+//função para calcular raiz quadrada.
+
+function addSquare(x) {
+    function square(x){
+        return x * x;
+    }
+    let sqrA = square(a);
+    let sqrB = square(b);
+    return sqrA + sqrB;
+
+}
+
