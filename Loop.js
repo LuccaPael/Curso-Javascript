f5b803ab94bc9a3ec623ec6a3b7b2d593cc2c5f8
LuccaPael <luccapael@hotmail.com>
2023-04-10 08:38:47 -0400

    Loop For

---
+++ Loop.js
@@ -0,0 +1,38 @@
+let cores = ['preto', 'branco', 'azul', 'vermelho'];
+let frutas = ['maçã', 'laranja', 'banana', 'uva'];
+let times = ['Palmeiras', 'Flamengo', 'Santos']
+
+
+//Primeiro For:
+for (let n = 0; n < cores.length; n++) {
+    console.log(cores[n]);
+}
+
+
+//Segundo For:
+for (let i in frutas) {
+    console.log(cores[i]);
+}
+
+
+//Terceiro For:
+for (let time in times) {
+    console.log(time);
+}
+
+
+
+
+//Array com objetos
+let passaportes = [
+    {nome: 'Carlos', pais: 'Brasil'},
+    {nome: 'Peter', pais: 'EUA'},
+    {nome: 'Mario', país: 'Itália'}
+];
+
+
+for (let passaporte of passaportes){
+    console.log(passaporte.nome);
+}
+
+
