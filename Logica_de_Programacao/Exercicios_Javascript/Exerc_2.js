5a8de4f0ed4cae36275b4338ff15fbf9ce55147f
LuccaPael <luccapael@hotmail.com>
2023-05-15 21:31:42 -0400

    calculando_media

---
+++ Logica_de_Programacao/Exercicios_Javascript/Exerc_2.js
@@ -0,0 +1,15 @@
+//Suponha que temos um array de números e queremos calcular a média e o desvio padrão desses valores. Podemos efetuar esses mesmos cálculos no estilo funcional conciso, usando os métodos de array map() e reduce()
+
+
+// Primeiramente, define duas funções simples
+var sum = function(x,y) {
+    return x+y;
+};
+var square = function(x) {
+    return x*x;
+};
+
+// Então, usa essas funções com os métodos Array para calcular a média e o desvio padrão
+var data = [1,1,3,5,5];
+var mean = data.reduce(sum)/data.length;
+var deviations = data.map(function(x) { return x-mean; });
