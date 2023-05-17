557fb70302acefb674799ce28a693ce6dda388b6
LuccaPael <luccapael@hotmail.com>
2023-05-16 21:09:23 -0400

    Update Exerc_2.js

--- Logica_de_Programacao/Exercicios_Javascript/Exerc_2.js
+++ Logica_de_Programacao/Exercicios_Javascript/Exerc_2.js
@@ -2,14 +2,15 @@
 
 
 // Primeiramente, define duas funções simples
-var sum = function(x,y) {
-    return x+y;
-};
-var square = function(x) {
-    return x*x;
-};
+var sum = function(x,y) { return x+y; };
+var square = function(x) { return x*x; };
 
-// Então, usa essas funções com os métodos Array para calcular a média e o desvio padrão
-let data = [1,1,3,5,5];
-let mean = data.reduce(sum)/data.length;
-let deviations = data.map(function(x) { return x-mean; });
+// Calculando a média
+var data = [4,6,8,10,12];
+var mean = data.reduce(sum)/data.length;
+console.log(mean);
+// Calculando o desvio padrão
+var deviations = data.map(function(x) {return x-mean;});
+var stddev = Math.sqrt(deviations.map(square).reduce(sum)/(data.length-1));
+
+console.log(stddev);
